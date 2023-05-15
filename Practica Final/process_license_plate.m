e69f6f0555b319aebdc90bd111fba6a8c640e1a3
Joan López Ferrer <trombonet12@gmail.com>
2023-05-15 17:27:21 +0200

    Fase inicial a mitges

--- Practica Final/process_license_plate.m
+++ Practica Final/process_license_plate.m
@@ -20,15 +20,8 @@ function matricula = process_license_plate(imagen)
     props = regionprops(E, 'Centroid');
 
     % Mostrar la imagen original y los bordes
-    subplot(1,2,2), imshow(imagen), title('Lineas exteriores y Centroides');
+    subplot(1,2,2), imshow(imagen), title('Lineas exteriores con Hough y Centroides');
     hold on;
-
-    % Dibujar los bordes de la imagen
-    B = bwboundaries(E);
-    for k = 1:length(B)
-        boundary = B{k};
-        plot(boundary(:,2), boundary(:,1), 'g', 'LineWidth', 2)
-    end
 
     % Dibujar los centroides de los caracteres
     for k = 1:length(props)
@@ -36,25 +29,35 @@ function matricula = process_license_plate(imagen)
         plot(c(1), c(2), 'r.', 'MarkerSize', 10);
     end
 
-    hold off,
+    % Detect the outer lines of the license plate using Hough transform
+    [H,theta,rho] = hough(E);
+    P = houghpeaks(H,2,'threshold',ceil(0.3*max(H(:))));
+    lines = houghlines(E,theta,rho,P,'FillGap',50,'MinLength',100);
+
+    % Plot the outer lines on the original image
+    for k = 1:length(lines)
+        xy = [lines(k).point1; lines(k).point2];
+        plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+    end
+    hold off;
 
     %% A figure showing the image after a thresholding, the skeletons of the letters and the endpoints/branchpoints of the letters
     % Umbralización para resaltar las letras
-    level = graythresh(I);
-    BW = imbinarize(I, level);
-
-    % Encuentra el esqueleto de las letras
-    skeleton = bwmorph(BW, 'skel', Inf);
-
-    % Encuentra los puntos finales y puntos de bifurcación de las letras
-    endpoints = bwmorph(skeleton, 'endpoints');
-    branchpoints = bwmorph(skeleton, 'branchpoints');
-    
-    % Muestra los resultados
-    figure('Name', 'Umbralización, Esqueleto, Puntos Finales y Puntos de Bifurcación');
-    subplot(1,3,1), imshow(BW), title('Imagen Umbralizada');
-    subplot(1,3,2), imshow(skeleton), title('Esqueleto');
-    subplot(1,3,3), imshow(endpoints + branchpoints), title('Puntos Finales y Puntos de Bifurcación');
+%     level = graythresh(I);
+%     BW = imbinarize(I, level);
+% 
+%     % Encuentra el esqueleto de las letras
+%     skeleton = bwmorph(BW, 'skel', Inf);
+% 
+%     % Encuentra los puntos finales y puntos de bifurcación de las letras
+%     endpoints = bwmorph(skeleton, 'endpoints');
+%     branchpoints = bwmorph(skeleton, 'branchpoints');
+%     
+%     % Muestra los resultados
+%     figure('Name', 'Umbralización, Esqueleto, Puntos Finales y Puntos de Bifurcación');
+%     subplot(1,3,1), imshow(BW), title('Imagen Umbralizada');
+%     subplot(1,3,2), imshow(skeleton), title('Esqueleto');
+%     subplot(1,3,3), imshow(endpoints + branchpoints), title('Puntos Finales y Puntos de Bifurcación');
 
     %% Output
     matricula = 1;
